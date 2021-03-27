1b6f5c691ee651f15213671dfdaa11783d2e8014
Ashley <ashleychang96@gmail.com>
2021-03-26 18:59:33 -0700

    Implement local storage save and populate functions

--- script.js
+++ script.js
@@ -5,21 +5,50 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update total and count
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    
+    const seatsIndex = [...selectedSeats].map((seat)=>[...seats].indexOf(seat));
+    
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
-    console.log(total);
-    console.log(selectedSeatsCount * ticketPrice);
+}
+
+// Get data from local storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }
 
 // Movie select event
-movieSelect.addEventListener('change', (e)=>{
+movieSelect.addEventListener('change', e =>{
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
-})
+});
 
 // Seat click event
 container.addEventListener('click', (e)=> {
@@ -28,7 +57,8 @@ container.addEventListener('click', (e)=> {
         e.target.classList.toggle('selected');
 
         updateSelectedCount();
-
-        console.log(total);
     }
 });
+
+// Initial count and total set on load
+updateSelectedCount();
